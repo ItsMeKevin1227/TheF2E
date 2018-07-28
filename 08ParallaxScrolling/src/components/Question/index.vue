b357b14d1a825a2da292793f62d98e650f351dd5
ItsMeKevin1227 <c.k.louis1227@gmail.com>
2018-07-28 12:40:41 +0800

    08 ParallaxScrolling
    最後結果只有三角形

---
+++ 08ParallaxScrolling/src/components/Question/index.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: 'question',
+  data() {
+    return {
+      show: false,
+    };
+  },
+  mounted() {
+    this.show = true;
+  },
+  props: ['detail', 'options'],
+  methods: {
+    onClick() {
+      this.$router.push(this.detail.nextPage);
+    },
+  },
+};
+</script>
+
+<template src="./template.html"></template>
+<style lang="scss" src="./style.scss" scoped></style>
